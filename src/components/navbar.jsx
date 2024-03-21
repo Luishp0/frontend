8b6b72ef91def03d8e690f7050bb7354b01fee24
Zu-R03 <carlos.csm222003@gmail.com>
2024-03-21 01:22:48 -0600

    Portada titulo,texto centrdo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../css/navbar.css';
 import { Link } from 'react-router-dom';
-import logo from "../img/logo.jpg"
+import logo from "../img/logo.jpg";
 
 const Navbar = () => {
   return (
-    <nav className="navbar" style={{ backgroundColor: 'white', padding: '10px', fontFamily: 'Roboto, sans-serif' }}>
+    <nav className="navbar" style={{ backgroundColor: '#ffffff', padding: '10px', fontFamily: 'Roboto, sans-serif' }}>
       <div className="logo">
         <img src={logo} alt="" style={{ marginRight: '10px', width: '30px', height: '30px' }} />
         <span style={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'black' }}>ACUACODE</span>
