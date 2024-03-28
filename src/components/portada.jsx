e8b116f66c11a4591c1a16754605039ead8b99b3
Zu-R03 <carlos.csm222003@gmail.com>
2024-03-28 16:40:29 -0600

    Portada

--- src/components/portada.jsx
+++ src/components/portada.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import "../css/portada.css";
-import foto1 from "../img/foto1.png"; // Importa la imagen que deseas mostrar dentro del círculo
-//hola
+import foto1 from "../img/foto1.png"; // Importa la imagen que deseas mostrar dentro del círculo interior
+
 const Portada = () => {
-  const titulo = "AcuaCode";
-  const textoDerecha = "Este es el texto a la derecha. Lorem ipsum dolor sit amet, consectetur adipiscing elit."; // Texto que irá a la derecha
   return (
     <div>
       <div className="portada-container">
-        <div className="outer-circle">
-          <div className="inner-circle">
-            <img src={foto1} alt="Imagen" className="circle-image" />
+        <div className="center-content">
+          <h2 className="titulo">AcuaCode</h2>
+          <p className="texto">AcuaCode es un software que maneja en su totalidad la pecera y ver el estado de salud de los peces</p>
+          <div className="botones-container">
+            <button className="boton">Registrarse</button>
+            <button className="boton">Iniciar Sesión</button>
+          </div>
+        </div>
+        <div className="circles-container">
+          <div className="circle-wrapper">
+            <div className="outer-circle">
+              <div className="inner-circle">
+                <img src={foto1} alt="Imagen" className="circle-image" />
+              </div>
+            </div>
+          </div>
+          <div className="second-circle-wrapper">
+            <div className="outer-circle second-circle">
+              <div className="inner-circle">
+                {/* Aquí puedes agregar la imagen del segundo círculo si lo deseas */}
+              </div>
+            </div>
           </div>
         </div>
       </div>
