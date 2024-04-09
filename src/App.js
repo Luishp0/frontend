bd7cf1d453075cc28068116f09903a4cdfba308f
LUIS-HP\soylu <soy.luis63@outloo.com>
2024-04-09 15:36:04 -0600

    borrando todos los css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Inicio from './components/inicio.jsx';
+
 import Login from './components/login.jsx'
-import Registro from './components/registro.jsx'
-import Navbar from './components/navbar.jsx';
-import RecuperacionC from './components/recuperacionC.jsx'
+
 import TablaUsuarios from './components/tablaUsuarios.jsx';  // Cambia la importación aquí
 
 function App() {
@@ -12,10 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/registro' element={<Registro />} />
-        <Route path='/inicio' element={<Inicio />} />
-        <Route path='/navbar' element={<Navbar />} />
-        <Route path='/recuperacionC' element={<RecuperacionC />} />
+        
         <Route path='/tablaUsuarios' element={<TablaUsuarios />} />  
       </Routes>
     </BrowserRouter>
