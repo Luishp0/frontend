ef6c506b73e262ef643b478148d4807c06141c0b
Zu-R03 <carlos.csm222003@gmail.com>
2024-08-03 01:21:49 -0600

    navbar y portada blancas

--- src/components/portada.jsx
+++ src/components/portada.jsx
@@ -5,7 +5,7 @@ import fondo21 from "../img/fondo21.jpg";
 
 const Portada = () => {
   const { darkMode } = useContext(AuthContext);
-  
+
   const pageStyle = {
     backgroundImage: `url(${fondo21})`,
     backgroundSize: 'cover',
@@ -15,15 +15,17 @@ const Portada = () => {
   };
 
   return (
-    <div className="pagina-container" style={pageStyle}>
+    <div className={`pagina-container ${darkMode ? 'bg-dark-bg' : 'bg-light-bg'}`} style={pageStyle}>
       <div className={`portada-container flex flex-col lg:flex-row justify-center items-center min-h-screen ${darkMode ? 'text-white' : 'text-black'}`}>
         <div className="text-container center-content text-center px-4 lg:w-1/2 lg:px-8 lg:mr-8 lg:mt-0 mt-0">
-          <h2 className="titulo text-5xl lg:text-6xl font-bold mb-6 lg:mb-10">AcuaCode</h2>
-          <p className="texto text-xl lg:text-2xl mb-8 lg:mb-10 text-justify">
+          <h2 className={`titulo text-5xl lg:text-6xl font-bold mb-6 lg:mb-10 ${darkMode ? 'text-black' : 'text-black'}`}>
+            AcuaCode
+          </h2>
+          <p className={`texto text-xl lg:text-2xl mb-8 lg:mb-10 text-justify ${darkMode ? 'text-black' : 'text-black'}`}>
             AcuaCode es un software completo para gestionar tu acuario y monitorear la salud de tus peces en tiempo real.
           </p>
-          <button className="boton leer-mas bg-cyan-400 text-white py-4 px-10 rounded-xl hover:bg-cyan-500 transition duration-300 text-lg">
-            <a href="#">Leer más</a>
+          <button className={`boton leer-mas bg-cyan-400 ${darkMode ? 'text-white' : 'text-black'} py-4 px-10 rounded-xl hover:bg-cyan-500 transition duration-300 text-lg`}>
+            <a href="#" className={`${darkMode ? 'text-white' : 'text-black'}`}>Leer más</a>
           </button>
         </div>
         <div className="image-container lg:w-1/2 lg:mr-0 lg:ml-auto lg:mt-0 mt-0 flex justify-center">
