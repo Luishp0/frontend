16793490fab905efb5a8eb2ebd8aabb4aed0854b
Zu-R03 <carlos.csm222003@gmail.com>
2024-08-03 01:11:58 -0600

    distancia de abajo de graficas

--- src/components/inicioAdministrador.jsx
+++ src/components/inicioAdministrador.jsx
@@ -7,25 +7,26 @@ import GraficaBarras from "./graficaBarras";
 
 const InicioAdministrador = () => {
     return (
-        <div className="flex h-screen overflow-hidden bg-gray-100">
-            <BarraLateral />
-            <div className="flex flex-col flex-grow p-4">
-                <Buscador />
-                <div className="bg-white rounded-lg shadow-md p-4 mb-4" style={{ marginTop: '2px' }}>
-                    <Tarjetas />
+        <div className="flex h-screen overflow-auto bg-gray-100"> {/* Cambiado overflow-hidden a overflow-auto */}
+    <BarraLateral />
+    <div className="flex flex-col flex-grow p-4">
+        <Buscador />
+        <div className="bg-white rounded-lg shadow-md p-4 mb-4" style={{ marginTop: '2px' }}>
+            <Tarjetas />
+        </div>
+        <div className="flex flex-col gap-4" style={{ marginTop: '1px' }}> {/* Añadido margen superior */}
+            <div className="flex flex-wrap gap-4 mb-4">
+                <div className="flex-1 min-w-[200px] bg-white rounded-lg shadow-md overflow-hidden">
+                    <GraficaBarras />
                 </div>
-                <div className="flex flex-col gap-4" style={{ marginTop: '1px' }}> {/* Añadido margen superior */}
-                    <div className="flex flex-wrap gap-4 mb-4">
-                        <div className="flex-1 min-w-[200px] bg-white rounded-lg shadow-md">
-                            <GraficaBarras />
-                        </div>
-                        <div className="flex-1 min-w-[200px] bg-white rounded-lg shadow-md">
-                            <GraficaPastel />
-                        </div>
-                    </div>
+                <div className="flex-1 min-w-[200px] bg-white rounded-lg shadow-md overflow-hidden">
+                    <GraficaPastel />
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
     )
 }
 
