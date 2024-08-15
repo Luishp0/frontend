5ddaf8961f099748bdb668e6a80cfb5297615080
LUIS-HP\soylu <soy.luis63@outloo.com>
2024-08-15 13:05:12 -0600

    tabla de peces creada

--- src/components/tablaPeceras.jsx
+++ src/components/tablaPeceras.jsx
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import BarraLateral from './barraLateral';
 import Buscador from './buscador';
 import { AuthContext } from './AuthContext'; // Asegúrate de tener la ruta correcta
 
-const datosUsuarios = [
-  {
-    nombre: "Diana Lucia Campos Estrada",
-    edad: 20,
-    pecera: "Pecera 1",
-    diseño: "Diseño 1",
-  },
-  {
-    nombre: "Luis",
-    edad: 25,
-    pecera: "Pecera 2",
-    diseño: "Diseño 2",
-  },
-];
-
 const TablaPeceras = () => {
   const { darkMode } = useContext(AuthContext);
+  const [ peces, setPeces ] = useState ([]); 
+
+  useEffect(() => {
+    fetchPeces();
+  }, []);
+
+  const fetchPeces = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/peces');
+      const data = await response.json();
+      setPeces(data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
 
   return (
     <div className={`flex h-screen ${darkMode ? 'bg-gray-800' : 'bg-gray-200'}`}>
@@ -34,27 +34,39 @@ const TablaPeceras = () => {
             <table className={`min-w-full ${darkMode ? 'bg-gray-700' : 'bg-white'}`}>
               <thead className={darkMode ? 'bg-gray-600' : 'bg-gray-50'}>
                 <tr>
+                  
                   <th className="px-6 py-3 border-b text-left text-xs font-medium uppercase tracking-wider">
-                    Nombre
+                    Tipo
+                  </th>
+                 
+                  <th className="px-6 py-3 border-b text-left text-xs font-medium uppercase tracking-wider">
+                    Alimentación
                   </th>
                   <th className="px-6 py-3 border-b text-left text-xs font-medium uppercase tracking-wider">
-                    Edad
-                  </th>
-                  <th className="px-6 py-3 border-b text-left text-xs font-medium uppercase tracking-wider">
-                    Pecera
-                  </th>
-                  <th className="px-6 py-3 border-b text-left text-xs font-medium uppercase tracking-wider">
-                    Diseño
+                    Parámetros
                   </th>
                 </tr>
               </thead>
               <tbody className={darkMode ? 'bg-gray-700 divide-y divide-gray-600' : 'bg-white divide-y divide-gray-200'}>
-                {datosUsuarios.map((usuario, index) => (
+                {peces.map((pez, index) => (
                   <tr key={index} className={`hover:${darkMode ? 'bg-gray-600' : 'bg-gray-100'} transition duration-200`}>
-                    <td className="px-6 py-4 text-sm">{usuario.nombre}</td>
-                    <td className="px-6 py-4 text-sm">{usuario.edad}</td>
-                    <td className="px-6 py-4 text-sm">{usuario.pecera}</td>
-                    <td className="px-6 py-4 text-sm">{usuario.diseño}</td>
+                    
+                    <td className="px-6 py-4 text-sm">{pez.tipo}</td>
+                   
+                    <td className="px-6 py-4 text-sm">
+                      {pez.alimentacion.map((alimento, i) => (
+                        <div key={i}>
+                          <strong>{alimento.alimento}</strong>: {alimento.cantidad}g ({alimento.frecuencia})
+                        </div>
+                      ))}
+                    </td>
+                    <td className="px-6 py-4 text-sm">
+                      {pez.parametros.map((parametro, i) => (
+                        <div key={i}>
+                          <strong>{parametro.nombre}</strong>: {parametro.valorSugerido}
+                        </div>
+                      ))}
+                    </td>
                   </tr>
                 ))}
               </tbody>
