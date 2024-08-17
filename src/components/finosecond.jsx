630c81aba8fe4b9b2a2669f9eb3169a2bc6c1e02
Zu-R03 <carlos.csm222003@gmail.com>
2024-08-17 01:05:25 -0600

    arreglo

--- src/components/finosecond.jsx
+++ src/components/finosecond.jsx
@@ -32,10 +32,11 @@ const Finosecond = () => {
               Betta Splendens (Pez Betta)
             </a>
             <p className={`mt-2 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-              Tamaño: Pequeño.
-              Temperamento: A menudo agresivo hacia otros peces betta y peces con colores brillantes similares.
-              Requisitos del agua: Prefieren agua cálida y limpia, con una temperatura de alrededor de 24-28°C.
-              Alimentación: Carnívoro, se alimenta de alimentos vivos y secos, como pellets de calidad para bettas.
+            Tamaño: Pequeño.
+Temperamento: A menudo agresivo hacia otros peces betta y peces con colores brillantes similares.
+Requisitos del agua: Prefieren agua cálida y limpia, con una temperatura de alrededor de 24-28°C.
+Alimentación: Carnívoro, se alimenta de alimentos vivos y secos, como pellets de calidad para bettas.
+
             </p>
           </div>
         </div>
@@ -47,7 +48,7 @@ const Finosecond = () => {
             <img
               className="h-48 w-full object-cover md:h-full md:w-48"
               src={pez2}
-              alt="Pez Payaso"
+              alt="Guppy"
             />
           </div>
           <div className={`p-8 ${darkMode ? 'text-white' : 'text-black'}`}>
@@ -58,13 +59,14 @@ const Finosecond = () => {
               href="#"
               className={`block mt-1 text-lg leading-tight font-medium hover:underline ${darkMode ? 'text-white' : 'text-black'}`}
             >
-              Amphiprioninae (Pez Payaso)
+              Guppy
             </a>
             <p className={`mt-2 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-              Tamaño: Pequeño a mediano.
-              Temperamento: Generalmente pacífico pero puede ser territorial.
-              Requisitos del agua: Prefieren agua salada y limpia, con una temperatura de alrededor de 24-28°C.
-              Alimentación: Omnívoro, se alimenta de alimentos variados incluyendo algas y pequeños invertebrados.
+            Tamaño: Pequeño.
+Temperamento: Pacífico, generalmente compatible con otros peces comunitarios.
+Requisitos del agua: Prefieren agua ligeramente alcalina y bien oxigenada, con una temperatura de alrededor de 24-28°C.
+Alimentación: Omnívoro, se alimenta de alimentos vivos y secos, como escamas y alimentos congelados.
+
             </p>
           </div>
         </div>
@@ -76,7 +78,7 @@ const Finosecond = () => {
             <img
               className="h-48 w-full object-cover md:h-full md:w-48"
               src={pez3}
-              alt="Pez Betta"
+              alt="Tetra Neón"
             />
           </div>
           <div className={`p-8 ${darkMode ? 'text-white' : 'text-black'}`}>
@@ -87,13 +89,13 @@ const Finosecond = () => {
               href="#"
               className={`block mt-1 text-lg leading-tight font-medium hover:underline ${darkMode ? 'text-white' : 'text-black'}`}
             >
-              Betta Splendens (Pez Betta)
+              Tetra Neón
             </a>
             <p className={`mt-2 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-              Tamaño: Pequeño.
-              Temperamento: A menudo agresivo hacia otros peces betta y peces con colores brillantes similares.
-              Requisitos del agua: Prefieren agua cálida y limpia, con una temperatura de alrededor de 24-28°C.
-              Alimentación: Carnívoro, se alimenta de alimentos vivos y secos, como pellets de calidad para bettas.
+            Tamaño: Pequeño.
+Temperamento: Pacífico, se lleva bien con otros peces comunitarios.
+Requisitos del agua: Prefieren agua suave y ligeramente ácida, con una temperatura de alrededor de 23-26°C.
+Alimentos vivos o congelados: Artemia, larvas de mosquito, y dafnias son ideales para complementar su dieta y mantener su colorido brillante.
             </p>
           </div>
         </div>
@@ -105,7 +107,7 @@ const Finosecond = () => {
             <img
               className="h-48 w-full object-cover md:h-full md:w-48"
               src={pez4}
-              alt="Pez Betta"
+              alt="Pez Ángel"
             />
           </div>
           <div className={`p-8 ${darkMode ? 'text-white' : 'text-black'}`}>
@@ -116,13 +118,13 @@ const Finosecond = () => {
               href="#"
               className={`block mt-1 text-lg leading-tight font-medium hover:underline ${darkMode ? 'text-white' : 'text-black'}`}
             >
-              Betta Splendens (Pez Betta)
+              Pez Ángel
             </a>
             <p className={`mt-2 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-              Tamaño: Pequeño.
-              Temperamento: A menudo agresivo hacia otros peces betta y peces con colores brillantes similares.
-              Requisitos del agua: Prefieren agua cálida y limpia, con una temperatura de alrededor de 24-28°C.
-              Alimentación: Carnívoro, se alimenta de alimentos vivos y secos, como pellets de calidad para bettas.
+            Tamaño: Mediano.
+Temperamento: Generalmente pacífico, pero puede volverse agresivo con otros peces de su misma especie.
+Requisitos del agua: Prefieren agua ligeramente ácida y bien oxigenada, con una temperatura de alrededor de 24-30°C.
+Alimentación: Omnivoros, se alimentan de una variedad de alimentos vivos, secos y congelados.
             </p>
           </div>
         </div>
@@ -134,7 +136,7 @@ const Finosecond = () => {
             <img
               className="h-48 w-full object-cover md:h-full md:w-48"
               src={pez5}
-              alt="Pez Betta"
+              alt="Pez Disco"
             />
           </div>
           <div className={`p-8 ${darkMode ? 'text-white' : 'text-black'}`}>
@@ -145,13 +147,13 @@ const Finosecond = () => {
               href="#"
               className={`block mt-1 text-lg leading-tight font-medium hover:underline ${darkMode ? 'text-white' : 'text-black'}`}
             >
-              Betta Splendens (Pez Betta)
+              Pez Disco
             </a>
             <p className={`mt-2 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-              Tamaño: Pequeño.
-              Temperamento: A menudo agresivo hacia otros peces betta y peces con colores brillantes similares.
-              Requisitos del agua: Prefieren agua cálida y limpia, con una temperatura de alrededor de 24-28°C.
-              Alimentación: Carnívoro, se alimenta de alimentos vivos y secos, como pellets de calidad para bettas.
+            Tamaño: Mediano.
+Temperamento: Pacífico, se lleva bien con otros peces tranquilos y pacíficos.
+Requisitos del agua: Prefieren agua blanda y ligeramente ácida, con una temperatura de alrededor de 26-32°C.
+Alimentación: Omnívoro, se alimenta de una variedad de alimentos vivos, congelados y secos.
             </p>
           </div>
         </div>
@@ -163,7 +165,7 @@ const Finosecond = () => {
             <img
               className="h-48 w-full object-cover md:h-full md:w-48"
               src={pez6}
-              alt="Pez Betta"
+              alt="Pez Globo Enano"
             />
           </div>
           <div className={`p-8 ${darkMode ? 'text-white' : 'text-black'}`}>
@@ -174,13 +176,13 @@ const Finosecond = () => {
               href="#"
               className={`block mt-1 text-lg leading-tight font-medium hover:underline ${darkMode ? 'text-white' : 'text-black'}`}
             >
-              Betta Splendens (Pez Betta)
+              Pez Globo Enano
             </a>
             <p className={`mt-2 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-              Tamaño: Pequeño.
-              Temperamento: A menudo agresivo hacia otros peces betta y peces con colores brillantes similares.
-              Requisitos del agua: Prefieren agua cálida y limpia, con una temperatura de alrededor de 24-28°C.
-              Alimentación: Carnívoro, se alimenta de alimentos vivos y secos, como pellets de calidad para bettas.
+            Tamaño: Pequeño.
+Temperamento: Pacífico, pero puede ser territorial.
+Requisitos del agua: Prefieren agua ligeramente alcalina y bien oxigenada, con una temperatura de alrededor de 24-28°C.
+Alimentación: Carnívoro, se alimenta de alimentos vivos y congelados, como camarones, mejillones y gusanos.
             </p>
           </div>
         </div>
